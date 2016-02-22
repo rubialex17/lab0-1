f019217d0e16050a3fcd6f8181b88d5e450a9a06
Alex <610841@unizar.es>
2016-02-22 09:15:55 +0100

    Modificadas las funciones necesarias para la práctica 0

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -2,12 +2,24 @@ $(document).ready(function() {
 	registerSearch();
 });
 
+//function registerSearch() {
+//	$("#search").submit(function(ev){
+//		event.preventDefault();
+//		$.get($(this).attr('action'), {q: $("#q").val()}, function(data) {
+//			$("#resultsBlock").empty().append(data);
+//			
+//		});	
+//	});
+//}
 function registerSearch() {
 	$("#search").submit(function(ev){
 		event.preventDefault();
-		$.get($(this).attr('action'), {q: $("#q").val()}, function(data) {
-			$("#resultsBlock").empty().append(data);
-		});	
-	});
+		
+		$.get($(this).attr('action'), {q: $("#q").val()}, function(data){
+			var template = $('#template').html();
+			var rendered = Mustache.to_html(template, data);   // optional, speeds up future uses
+		
+		$("#resultsBlock").empty().append(rendered);
+		});
+		});
 }
-
